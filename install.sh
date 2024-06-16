bc0fa984f03439294deaa24f417a299ad0917d42
Ramil <ramil345655@gmail.com>
2024-06-16 08:02:22 +0300

    Sun Jun 16 08:02:22 AM MSK 2024

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-pacman -S xorg-server xorg-xinit xorg-xset i3-wm pulseaudio alsa-utils git github-cli
+pacman -S xorg-server xorg-xinit xorg-xset \
+	i3-wm pulseaudio alsa-utils git github-cli\
+	noto-fonts noto-fonts-emoji
 yay -S polybar
 amixer sset Master 80% unmute
 gh auth login
